10a81cb5ed16d1010ec5ace68830678de3a52452
unknown <eivind.rydningen@gmail.com>
2022-05-20 13:09:10 +0200

    Clean code and final version of prototype

--- src/app.rs
+++ src/app.rs
@@ -1,14 +1,14 @@
+use iota_streams::app::transport::tangle::client::Client;
 
 #[path = "iota.rs"] mod iota;
 
-/// We derive Deserialize/Serialize so we can persist app state on shutdown.
-#[derive(serde::Deserialize, serde::Serialize)]
-#[serde(default)] // if we add new fields, give them default values when deserializing old state
 pub struct TemplateApp {
-    // Example stuff:
+    client: Client,
+    health_data: Vec<String>,
+    ann_link: String,
+    sub_link: String,
+    keyload_link: String,
     author_seed: String,
-    author_ann_address: String,
-    sub_ann_address: String,
     sub_seed: String,
     response: String,
 }
@@ -16,10 +16,12 @@ pub struct TemplateApp {
 impl Default for TemplateApp {
     fn default() -> Self {
         Self {
-            // Example stuff:
+            client: Client::new_from_url("https://api.lb-0.h.chrysalis-devnet.iota.cafe/"),
+            health_data: vec![],
+            ann_link: "Dummy Announcement Link".to_owned(),
+            sub_link: "Dummy Sub Link".to_owned(),
+            keyload_link: "Dummy Key Link".to_owned(),
             author_seed: "Enter Author Seed".to_owned(),
-            author_ann_address: "Dummy Author Address".to_owned(),
-            sub_ann_address: "Dummy Sub Address".to_owned(),
             sub_seed: "Enter Sub Seed".to_owned(),
             response: "Waiting".to_owned(),
         }
@@ -27,39 +29,17 @@ impl Default for TemplateApp {
 }
 
 impl TemplateApp {
-    /// Called once before the first frame.
-    pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
-        // This is also where you can customized the look at feel of egui using
-        // `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
-
-        // Load previous app state (if any).
-        // Note that you must enable the `persistence` feature for this to work.
-        if let Some(storage) = cc.storage {
-            return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
-        }
-
+    pub fn new(_cc: &eframe::CreationContext<'_>) -> Self {
         Default::default()
     }
 }
 
 impl eframe::App for TemplateApp {
-    /// Called by the frame work to save state before shutdown.
-    fn save(&mut self, storage: &mut dyn eframe::Storage) {
-        eframe::set_value(storage, eframe::APP_KEY, self);
-    }
 
-    /// Called each time the UI needs repainting, which may be many times per second.
-    /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
     fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-        let Self { author_seed, author_ann_address, sub_ann_address, sub_seed, response } = self;
-
-        // Examples of how to create different panels and windows.
-        // Pick whichever suits you.
-        // Tip: a good default choice is to just keep the `CentralPanel`.
-        // For inspiration and more examples, go to https://emilk.github.io/egui
+        let Self { client, health_data, ann_link, sub_link, keyload_link, author_seed, sub_seed, response } = self;
 
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
-            // The top panel is often a good place for a menu bar:
             egui::menu::bar(ui, |ui| {
                 ui.menu_button("File", |ui| {
                     if ui.button("Quit").clicked() {
@@ -71,28 +51,68 @@ impl eframe::App for TemplateApp {
 
         egui::SidePanel::left("left_side_panel").show(ctx, |ui| {
             ui.heading("Author Panel");
-            ui.label("Enter your author seed and click to create new iota streams channel");
+            ui.label("");
+            ui.label("Enter your author seed and click to create new IOTA Streams channel");
+            ui.label("");
+            ui.label("Author Seed:");
             ui.text_edit_singleline(author_seed);
             if ui.button("Create Channel").clicked() {
-                *author_ann_address = iota::create_channel(author_seed.to_string()).unwrap_or_default();
+                *ann_link = iota::create_channel(author_seed.to_owned(), client.to_owned()).unwrap_or_default();
             }
-            ui.text_edit_singleline(author_ann_address);
-        });
-
-        egui::SidePanel::right("right_side_panel").show(ctx, |ui| {
-            ui.heading("Subscriber Panel");
-            ui.label("Enter the announcement link and your seed, then click to subscribe to the IOTA streams channel");
-            ui.text_edit_singleline(sub_seed);
-            ui.text_edit_singleline(sub_ann_address);
-            if ui.button("Subscribe To Channel").clicked() {
-                *response = iota::subscribe_to_channel(sub_seed.to_string(), sub_ann_address.to_string()).unwrap_or_default();
+            ui.label("");
+            ui.label("Announcement Link:");
+            ui.text_edit_singleline(ann_link);
+            ui.label("");
+            ui.label("");
+            ui.label("Enter subscription link to process subscriber");
+            ui.label("");
+            ui.label("Subscription Link:");
+            ui.text_edit_singleline(sub_link);
+            if ui.button("Process Subscription").clicked() {
+                *keyload_link = iota::process_subscription(author_seed.to_owned(), ann_link.to_owned(), sub_link.to_owned(), client.to_owned()).unwrap_or_default();
+            }
+            ui.label("");
+            ui.label("Keyload Link:");
+            ui.text_edit_singleline(keyload_link);
+            ui.label("");
+            ui.label("");
+            if ui.button("Send Health Data").clicked() {
+                *response = iota::send_health_data(author_seed.to_owned(), keyload_link.to_owned(), client.to_owned()).unwrap_or_default();
             }
             ui.text_edit_singleline(response);
         });
 
+        egui::SidePanel::right("right_side_panel").show(ctx, |ui| {
+            ui.heading("Subscriber Panel");
+            ui.label("");
+            ui.label("Enter your seed and the announcement link, then click to subscribe to the IOTA streams channel");
+            ui.label("");
+            ui.label("Subscriber Seed:");
+            ui.text_edit_singleline(sub_seed);
+            ui.label("Announcement Link:");
+            ui.text_edit_singleline(ann_link);
+            if ui.button("Subscribe To Channel").clicked() {
+                *sub_link = iota::subscribe_to_channel(sub_seed.to_owned(), ann_link.to_owned(), client.to_owned()).unwrap_or_default();
+            }
+            ui.label("");
+            ui.label("Subscription Link:");
+            ui.text_edit_singleline(sub_link);
+            ui.label("");
+            ui.label("");
+            if ui.button("Fetch and Process Health Data").clicked() {
+                *health_data = iota::fetch_health_data(sub_seed.to_owned(), client.to_owned()).unwrap_or_default(); 
+            }
+        });
+
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading("Health Data Stream");
-            egui::warn_if_debug_build(ui);
+            ui.label("");
+            ui.label("  Private data received by Subscriber from Author");
+            ui.label("");
+            for msg in health_data {
+                ui.label(msg.to_string());
+                ui.label("");
+            }
         });
 
         if false {
--- src/iota.rs
+++ src/iota.rs
@@ -1,34 +1,163 @@
-use iota_streams::app_channels::api::tangle::{ Address, Author, Subscriber, ChannelType};
+use iota_streams::app_channels::api::tangle::{ Address, Author, Bytes, Subscriber, ChannelType, MessageContent };
 use iota_streams::app::transport::tangle::client::Client;
 use anyhow::Result;
-use std::str::FromStr;
+use core::str::FromStr;
 
 #[tokio::main]
-pub async fn create_channel(seed: String) -> Result<String> {
-    let node = "https://api.lb-0.h.chrysalis-devnet.iota.cafe/";
-    let client = Client::new_from_url(node);
+pub async fn create_channel(author_seed: String, client: Client) -> Result<String> {
+    let mut author = Author::new(&author_seed, ChannelType::SingleBranch, client);
 
-    let mut author = Author::new(&seed, ChannelType::SingleBranch, client);
+    let ann_link = author.send_announce().await?;
 
-    let ann_address = author.send_announce().await?;
-
-    Ok(ann_address.to_string())
+    Ok(ann_link.to_string())
 }
 
 #[tokio::main]
-pub async fn subscribe_to_channel(seed: String, ann_address: String) -> Result<String> {
+pub async fn subscribe_to_channel(sub_seed: String, ann_link: String, client: Client) -> Result<String> {
+    let mut subscriber = Subscriber::new(&sub_seed, client);
+
+    let ann_link_adr = Address::from_str(&ann_link)?;
+
+    subscriber.receive_announcement(&ann_link_adr).await?;
+
+    let sub_link = subscriber.send_subscribe(&ann_link_adr).await?;
+
+    Ok(sub_link.to_string())
+}
+
+#[tokio::main]
+pub async fn process_subscription(author_seed: String, ann_link: String, sub_link: String, client: Client) -> Result<String> {
+    let mut author = Author::new(&author_seed, ChannelType::SingleBranch, client);
+
+    let ann_link_adr = Address::from_str(&ann_link)?;
+
+    let sub_link_adr = Address::from_str(&sub_link)?;
+    
+    author.receive_subscribe(&sub_link_adr).await?;
+
+    let (keyload_link, _seq) = author.send_keyload_for_everyone(&ann_link_adr).await?;
+
+    Ok(keyload_link.to_string())
+}
+
+#[tokio::main]
+pub async fn send_health_data(author_seed: String, keyload_link: String, client: Client) -> Result<String> {
+    let mut author = Author::new(&author_seed, ChannelType::SingleBranch, client.clone());
+
+    let msg_inputs = vec![
+        "HR:  80  -  Steps:  2000  -  Calories:  1765", "HR:  78  -  Steps:  2111  -  Calories:  1803", 
+        "HR:  74  -  Steps:  2245  -  Calories:  1876", "HR:  85  -  Steps:  2302  -  Calories:  1920",
+        "HR:  72  -  Steps:  2340  -  Calories:  1997", "HR:  86  -  Steps:  2360  -  Calories:  2002",
+        "HR:  74  -  Steps:  2400  -  Calories:  2103", "HR:  67  -  Steps:  2410  -  Calories:  1920",
+    ];
+
+    let mut prev_msg_link = Address::from_str(&keyload_link)?;
+
+    for input in &msg_inputs {
+        let (msg_link, _seq_link) = author.send_signed_packet(
+            &prev_msg_link,
+            &Bytes::default(),
+            &Bytes(input.as_bytes().to_vec()),
+        ).await?;
+        println!("Sent msg: {}, tangle index: {:#}", msg_link, msg_link.to_msg_index());
+        prev_msg_link = msg_link;
+    }
+    
+    Ok("Health Data Sent".to_string())
+}
+
+#[tokio::main]
+pub async fn fetch_health_data(sub_seed: String, client: Client) -> Result<Vec<String>> {
+
+    let mut subscriber = Subscriber::new(&sub_seed, client);
+
+    let retrieved_msgs = subscriber.fetch_next_msgs().await;
+
+    let messages = retrieved_msgs.unwrap_or_default();
+
+    let processed_msgs = messages
+        .iter()
+        .map(|msg| {
+            let content = &msg.body;
+            match content {
+                MessageContent::SignedPacket {
+                    pk: _,
+                    public_payload: _,
+                    masked_payload,
+                } => String::from_utf8(masked_payload.0.to_vec()).unwrap(),
+                _ => String::default(),
+            }
+        })
+        .filter(|s| s != &String::default())
+        .collect::<Vec<String>>();
+    
+    Ok(processed_msgs)
+}
+
+// Run all IOTA Streams functions
+#[tokio::main]
+pub async fn run_all(seed: String) -> Result<Vec<String>> {
+    
+    // ----------------------- Create Author instance from unique seed -----------------------
     let node = "https://api.lb-0.h.chrysalis-devnet.iota.cafe/";
     let client = Client::new_from_url(node);
+    let mut author = Author::new(&seed, ChannelType::SingleBranch, client.clone());
 
-    let mut subscriber = Subscriber::new(&seed, client);
+    // Send announcement of newly created channel, returns announcement link
+    let ann_link = author.send_announce().await?;
+
+    // ----------------------- Create Subscriber instance from unique seed -----------------------
+    let mut subscriber = Subscriber::new("SubscriberA433", client);
+
+    subscriber.receive_announcement(&ann_link).await?;
+
+    let sub_address = subscriber.send_subscribe(&ann_link).await?;
     
-    // Create Address object from announcement address string
-    let ann_address = Address::from_str(&ann_address.to_string())?;   
-    //println!("{}", ann_address.to_string());
+    // ----------------------- Author processes the subscription -----------------------
+    author.receive_subscribe(&sub_address).await?;
 
-    // Process the announcement message
-    subscriber.receive_announcement(&ann_address).await?;
+    let (keyload_link, _seq) = author.send_keyload_for_everyone(&ann_link).await?;
 
+    // ----------------------- Author sends health data as a vector of strings
+    let msg_inputs = vec![
+        "HR:  80  -  Steps:  2000  -  Calories:  1765", "HR:  78  -  Steps:  2111  -  Calories:  1803", 
+        "HR:  74  -  Steps:  2245  -  Calories:  1876", "HR:  85  -  Steps:  2302  -  Calories:  1920",
+        "HR:  72  -  Steps:  2340  -  Calories:  1997", "HR:  86  -  Steps:  2360  -  Calories:  2002",
+        "HR:  74  -  Steps:  2400  -  Calories:  2103", "HR:  67  -  Steps:  2410  -  Calories:  1920",
+    ];
 
-    Ok("Success".to_owned())
+    let mut prev_msg_link = keyload_link;
+
+    for input in &msg_inputs {
+        let (msg_link, _seq_link) = author.send_signed_packet(
+            &prev_msg_link,
+            &Bytes::default(),
+            &Bytes(input.as_bytes().to_vec()),
+        ).await?;
+        println!("Sent msg: {}, tangle index: {:#}", msg_link, msg_link.to_msg_index());
+        prev_msg_link = msg_link;
+    }
+
+    // ----------------------- Subscriber fetches health data sent by the Author -----------------------
+    let retrieved_msgs = subscriber.fetch_next_msgs().await;
+
+    let messages = retrieved_msgs.unwrap_or_default();
+
+    let processed_msgs = messages
+        .iter()
+        .map(|msg| {
+            let content = &msg.body;
+            match content {
+                MessageContent::SignedPacket {
+                    pk: _,
+                    public_payload: _,
+                    masked_payload,
+                } => String::from_utf8(masked_payload.0.to_vec()).unwrap(),
+                _ => String::default(),
+            }
+        })
+        .filter(|s| s != &String::default())
+        .collect::<Vec<String>>();
+
+    Ok(processed_msgs)
 }
